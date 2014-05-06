de5fc86a90a5131c10e26e2326e960acd94487e0
Diego Fernández Barrera <bigomby@gmail.com>
2014-05-06 11:08:56 +0200

    Intentando que funcione el SNMP

--- src/MultiThreadedTrapReceiver.java
+++ src/MultiThreadedTrapReceiver.java
@@ -20,33 +20,30 @@
 
 
 
-import org.snmp4j.Snmp;
-import org.snmp4j.smi.Address;
-import org.snmp4j.security.SecurityProtocols;
-import org.snmp4j.smi.UdpAddress;
-import org.snmp4j.mp.MessageProcessingModel;
-import org.snmp4j.security.SecurityModels;
-import org.snmp4j.TransportMapping;
-import org.snmp4j.smi.OctetString;
-import org.snmp4j.transport.DefaultTcpTransportMapping;
-import org.snmp4j.mp.MPv3;
-import org.snmp4j.transport.DefaultUdpTransportMapping;
-import org.snmp4j.security.USM;
-import org.snmp4j.smi.GenericAddress;
-import java.net.UnknownHostException;
 import java.io.IOException;
-import org.snmp4j.util.MultiThreadedMessageDispatcher;
-import org.snmp4j.util.ThreadPool;
-import org.snmp4j.MessageDispatcherImpl;
-import org.snmp4j.smi.TcpAddress;
+import java.net.UnknownHostException;
+
 import org.snmp4j.CommandResponder;
 import org.snmp4j.CommandResponderEvent;
+import org.snmp4j.MessageDispatcherImpl;
+import org.snmp4j.Snmp;
+import org.snmp4j.TransportMapping;
 import org.snmp4j.mp.MPv1;
 import org.snmp4j.mp.MPv2c;
-import org.snmp4j.log.LogFactory;
-import org.snmp4j.log.Log4jLogFactory;
-import org.snmp4j.asn1.BER;
-import org.apache.log4j.BasicConfigurator;
+import org.snmp4j.mp.MPv3;
+import org.snmp4j.mp.MessageProcessingModel;
+import org.snmp4j.security.SecurityModels;
+import org.snmp4j.security.SecurityProtocols;
+import org.snmp4j.security.USM;
+import org.snmp4j.smi.Address;
+import org.snmp4j.smi.GenericAddress;
+import org.snmp4j.smi.OctetString;
+import org.snmp4j.smi.TcpAddress;
+import org.snmp4j.smi.UdpAddress;
+import org.snmp4j.transport.DefaultTcpTransportMapping;
+import org.snmp4j.transport.DefaultUdpTransportMapping;
+import org.snmp4j.util.MultiThreadedMessageDispatcher;
+import org.snmp4j.util.ThreadPool;
 
 public class MultiThreadedTrapReceiver implements CommandResponder {
 
