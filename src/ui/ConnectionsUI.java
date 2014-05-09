ed950dccde922fbc94eff118d6963dd4e78490c7
Diego Fernández Barera <bigomby@gmail.com>
2014-05-10 01:03:01 +0200

    Añadidas las clases para las conexiones.

---
+++ src/ui/ConnectionsUI.java
@@ -0,0 +1,17 @@
+package ui;
+
+import java.util.List;
+
+import connections.Connection;
+
+public class ConnectionsUI {
+
+	public ConnectionsUI(List<Connection> connections) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	void menu(){
+		// TODO
+	}
+
+}
