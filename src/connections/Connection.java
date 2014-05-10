17a677886b096fb17994bdf11e46a042b004d0c2
Diego Fernández Barera <bigomby@gmail.com>
2014-05-10 03:49:34 +0200

    Implementado el método forward.

--- src/connections/Connection.java
+++ src/connections/Connection.java
@@ -1,14 +1,34 @@
 package connections;
 
+import java.util.Iterator;
+import java.util.List;
+
 import services.Service;
 import agents.Agent;
 
 
 
 public class Connection {
+	
+	private List<Agent> agents;
+	private List<Service> services;
+	
+	public void forward(String ip, String message){
+		Iterator<Service> it = services.iterator();
+		Service service;
+		
+		while(it.hasNext()){
+			service = it.next();
+			service.pushMessage(ip, message);
+		}
+	}
 
 	void addAgent(Agent agent){
 		// TODO Crear el método para añadir agentes 
+	}
+
+	Agent getAgent(int index) {
+		return agents.get(index);
 	}
 	
 	void removeAgent(Agent agent){
